df8d92d096f49acae913608046eb0f7e7339d492
Lonnie Gerol <lonniegerol@gmail.com>
2020-01-05 16:42:02 -0600

    beggining work on network manager

---
+++ Drink/Manager/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  Drink
+//
+//  Created by Lonnie Gerol on 1/5/20.
+//  Copyright © 2020 Lonnie Gerol. All rights reserved.
+//
+
+import Foundation
+import AppAuth
+class NetworkManager{
+    static let shared = NetworkManager()
+    let baseURL = "https://mizu.csh.rit.edu:443"
+}
---
+++ Drink/Manager/SessionManager.swift
@@ -0,0 +1,14 @@
+//
+//  SessionManager.swift
+//  Drink
+//
+//  Created by Lonnie Gerol on 1/5/20.
+//  Copyright © 2020 Lonnie Gerol. All rights reserved.
+//
+
+import Foundation
+
+class SessionManager{
+    static let shared = SessionManager()
+    private init(){}
+}
