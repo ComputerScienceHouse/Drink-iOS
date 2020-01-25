5b86c8232aaa9473d11dec731849cc2214e3e729
Lonnie <Gerol>
2020-01-24 20:41:44 -0500

    added offline_access to scopes, hopefully fixing refresh token expiring issue

---
+++ Drink/Custom Views/MachineStatusView.swift
@@ -0,0 +1,21 @@
+//
+//  MachineStatusView.swift
+//  Drink
+//
+//  Created by Lonnie Gerol on 1/24/20.
+//  Copyright © 2020 Lonnie Gerol. All rights reserved.
+//
+
+import UIKit
+
+class MachineStatusView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
